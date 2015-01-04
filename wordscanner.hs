e8ae2ba3a3181121a28f9ab60e136ca5103512ef
Thomas Levine <_@thomaslevine.com>
2015-01-04 09:17:18 +0000

    -

--- wordscanner.hs
+++ wordscanner.hs
@@ -1,6 +1,10 @@
 module Wordscanner where
 
+import qualified Text.ParserCombinators.Parsec as P
+
 data Word = Name String |
             Literal String |
             Number String |
             SpecialCharacter String
+
+P.parse P.space "abc" "444"
