3708add3bc627c7a848c425ab865a0f89e223325
Thomas Levine <_@thomaslevine.com>
2015-01-04 10:33:48 +0000

    -

--- wordscanner.hs
+++ wordscanner.hs
@@ -22,8 +22,17 @@ name = do a <- nameHead
     nameHead = P.letter P.<|> (P.char '_')
     nameTail = P.many $ nameHead P.<|> P.digit
 
-literal :: P.Parser Word
-literal = do quotechar <- P.oneOf "\"'"
-             body <- P.many $ P.noneOf "\"" P.<|> 
-             P.char quotechar
-             return $ Literal body
+-- literal :: P.Parser Word
+literal = do quoteChar <- P.oneOf "\"'"
+             body <- P.many $
+                              normalString quoteChar  P.<|> 
+                                escaped quoteChar
+             P.char quoteChar
+             return $ body
+  where
+    escaped quoteChar = do x <- P.string _ -- [quoteChar, quoteChar]
+                           return quoteChar
+    normalString quoteChar = do x <- P.many $ P.noneOf [quoteChar]
+                                return $ x
+
+p t = P.parse t "(unspecified source)"
