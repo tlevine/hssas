fca93f07d1e8c6d799864037f9d343828ab824da
Thomas Levine <_@thomaslevine.com>
2015-01-04 09:29:19 +0000

    parseName

--- wordscanner.hs
+++ wordscanner.hs
@@ -7,4 +7,10 @@ data Word = Name String |
             Number String |
             SpecialCharacter String
 
-P.parse P.space "abc" "444"
+parseName :: P.Parser Word
+parseName = do a <- nameHead
+               b <- nameTail
+               return $ Name $ a:b
+  where
+    nameHead = P.letter P.<|> (P.char '_')
+    nameTail = P.many $ nameHead P.<|> P.digit
